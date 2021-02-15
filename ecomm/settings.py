5b3acb55936103540fe877012761d260a644b9fa
Vitor <vitorhugobrizante@gmail.com>
2021-02-14 21:11:02 -0300

    create context variable title and content to home_page, about_page, about_page and contact_page, create and set src/template/about/views.html, src/template/contact/view.html and update src/template/home_page.html to use context variables

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for ecomm project.
 
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'ecomm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
