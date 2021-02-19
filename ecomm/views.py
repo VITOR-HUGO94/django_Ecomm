f307bb62c18db87f85f62f21c2254575c50663ad
Vitor <vitorhugobrizante@gmail.com>
2021-02-19 17:17:55 -0300

    create cbv and fbv

--- ecomm/views.py
+++ ecomm/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import authenticate, login, get_user_model
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
+
 from .forms import ContactForm, LoginForm, RegisterForm
 
 def home_page(request):
